fa746ba364c033ddad948fc319d9636dc5dffd4b
feixiangsnail <feixiangsnail@hotmail.com>
2020-08-21 10:00:01 +0800

    init

---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from './home/home'
+const App: React.FC = ()=>{
+  return(
+    <div>
+      <h1>react Fc Demo</h1>
+      <Home />
+    </div>
+  )
+}
+export default App
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+export default class Home extends React.Component<IProps>{
+render(){
+    return (
+        <div>
+            <p>I  am  homePage</p>
+        </div>
+    )
+}
+}
